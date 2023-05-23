0a934ad16e86d166e076f53556a25aa822f1751d
Роман Носов <romannosov_2001@mail.ru>
2023-05-23 21:21:34 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static AtomicInteger beautiful3;
+    public static AtomicInteger beautiful4;
+    public static AtomicInteger beautiful5;
+
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+
+
+//        new Thread(() -> {  // abba
+//            for (String s : texts) {
+//                if (s.equals(new StringBuilder(s).reverse().toString())){
+//                    System.out.println(s);
+//                }
+//            }
+//        }).start();
+
+        new Thread(() -> {  // fff
+           for (String s : texts) {
+                char c = s.charAt(0);
+                boolean same = true;
+                for (char check : s.toCharArray()) {
+                    if (!(check == c)) {
+                        same = false;
+                    }
+                }
+                if (same){
+                    System.out.println(s);
+                }
+           }
+        }).start();
+
+//        new Thread(() -> {  // ppfff
+//           for (String s : texts) {
+//
+//           }
+//        });
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
